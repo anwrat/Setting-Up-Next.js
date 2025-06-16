2ed882914f4c315febb04b0ff5dfc1e87e314901
anwrat <anweshrwt22@gmail.com>
2025-06-16 08:27:25 +0545

    Clearing globals.css

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ export default function Home(){
   return(
     <div>
       <h1>Hello World</h1>
+      <p>This is my first NextJS project</p>
+      <ol>
+        <li>NextJS</li>
+      </ol>
     </div>
   )
 }
