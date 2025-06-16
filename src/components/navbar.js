79ae8a21a8b84a24034d12f8ed96b959b6bdd5a1
anwrat <anweshrwt22@gmail.com>
2025-06-16 21:36:04 +0545

    Adding simple styling to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,21 @@
 //For navigation in NextJS
 import Link from 'next/link';
+import Vercellogo from 'next/image';
 
 export default function Navbar(){
     return(
-        <div>
-            <Link href="/">Home Page</Link>
-            <Link href="/about">About Page</Link>
+        <div className='text-white bg-black mb-4'>
+            <div className='container flex items-center mx-auto justify-between'>
+                <div className='flex flex-col items-center'>
+                    <Vercellogo src = '/vercel.svg' alt = "Vercel logo" width={50} height={50}/>
+                    <span>Vercel</span>
+                </div>
+                <div className='flex gap-4'>
+                    <Link href="/" className='font-bold hover:underline'>Home</Link>
+                    <Link href="/about" className='font-bold hover:underline'>About</Link>
+                    <Link href="/contact" className='font-bold hover:underline'>Contact</Link>
+                </div>
+            </div>
         </div>
     );
 }
