ca1ac97da019bfb441c673ac27965fd83547e529
anwrat <anweshrwt22@gmail.com>
2025-06-22 17:39:19 +0545

    ColorChanging card using state

--- src/app/colorchange/page.js
+++ src/app/colorchange/page.js
@@ -1,8 +1,16 @@
+'use client';
+import { useState } from "react";
+
 export default function ColorChange(){
+    const [theme,changetheme] = useState("Light");
     return(
-        <div>
-            <p>Make a card here with a button which switches dark and light mode of the card</p>
-            <p>Make a dropdown menu in navbar with multilevel, dropdown after dropdown</p>
+        <div className="text-center">
+            <div className={`w-fit mx-auto p-4 text-center border-2 rounded-2xl ${theme === "Dark" ? "bg-black text-white" : "bg-white text-black"}`}>
+                <h1 className="font-bold text-2xl">Theme Toggle Card</h1>
+                <h1>Current Mode: {theme} Mode</h1>
+                <button onClick={()=>{changetheme(theme === "Light" ? "Dark" : "Light")}} className={`cursor-pointer p-2 rounded-md ${theme === "Light" ? "bg-black text-white" : "bg-white text-black"}`}>Switch to {theme === "Light" ? "Dark" : "Light"} Mode</button>
+                <h1>Click the button to change themes</h1>
+            </div>
         </div>
     );
 }
